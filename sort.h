42251aedb79e3b2216b61cece22304ccf2dbef0a
Bensonodhiambo <benodhiambo.bo@gmail.com>
2024-02-20 04:45:43 +0300

    Starter_files

---
+++ sort.h
@@ -0,0 +1,11 @@
+#ifndef SORT_FILE
+#define SORT_FILE
+
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct listint_s listint_t;
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+#endif
